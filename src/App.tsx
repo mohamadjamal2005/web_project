1ad9c99ee141d5f204d109e99698cbc72f74cdc5
Mohamad Jamal <oussgool33@gmail.com>
2026-05-21 01:03:53 +0300

    file modified

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,16 @@ import MainLayout from "./layouts/MainLayout";
 import Login from "./pages/auth/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 
+import Grades from "./pages/student/Grades";
+import Attendance from "./pages/student/Attendance";
+
+import Classes from "./pages/instructor/Classes";
+import Students from "./pages/instructor/Students";
+import Submissions from "./pages/instructor/Submissions";
+
+import Reports from "./pages/admin/Reports";
+import Settings from "./pages/admin/Settings";
+
 import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
@@ -34,6 +44,20 @@ const App = () => {
           path="*"
           element={<Navigate to="/login" />}
         />
+
+        <Route path="/grades" element={<Grades />} />
+
+        <Route path="/attendance" element={<Attendance />}/>
+
+        <Route path="/classes" element={<Classes />} />
+
+        <Route path="/students" element={<Students />} />
+
+        <Route path="/submissions" element={<Submissions />} />
+
+        <Route path="/reports" element={<Reports />} />
+
+        <Route path="/settings" element={<Settings />} />
 
       </Routes>
     </BrowserRouter>
