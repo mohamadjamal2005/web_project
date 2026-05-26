7001e32bbad65f67c31a67605afc21ecece5d94b
Mohamad Jamal <oussgool33@gmail.com>
2026-05-27 02:10:45 +0300

    last commit

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,16 +7,12 @@ const MainLayout = () => {
   return (
     <div className="min-h-screen flex bg-gray-100">
 
-      {/* Sidebar */}
       <Sidebar />
 
-      {/* Main Content */}
       <div className="flex-1 flex flex-col">
 
-        {/* Navbar */}
         <Navbar />
 
-        {/* Page Content */}
         <main className="p-8 flex-1">
           <Outlet />
         </main>
